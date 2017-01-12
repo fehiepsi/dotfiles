5c7c6d5c698624cd3b89f438f39c1af200b3a4cb
fehiepsi <fehiepsi@gmail.com>
2017-01-12 13:12:26 +0900

    fix typos

--- install.sh
+++ install.sh
@@ -60,7 +60,6 @@ conda create -n r -c r r-essentials
 sudo apt-get install gcc libcupti-dev 
 cd ~/Downloads
 curl -LO https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
-tar xvzf cuda_8.0.44_linux.run
 sudo sh cuda_8.0.44_linux.run # ignore driver and samples
 curl -LO https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v5.1/prod/8.0/cudnn-8.0-linux-x64-v5.1-tgz
 tar xvzf cudnn-8.0-linux-x64-v5.1-ga.tgz
